709088e3488dfc12f8f06629f680618f9a5db81e
Hanna Skeppe <hannaskeppe@gmail.com>
2020-10-18 18:14:33 +0200

    Added component

---
+++ src/DropDown.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const DropDown = props => {
+  return (
+    <>
+      <div class="dropdown">
+        <button class="dropbtn">Dropdown</button>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <div className="dropdown">
+        <button className="dropbtn">Dropdown</button>
+        <div className="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </>
+  )
+}
+export default DropDown
